41629da55a9358223d74f3eb479f04604f3a57b7
JOJO\38097 <kartemoko@gmail.com>
2021-07-11 19:20:09 +0300

    task1

---
+++ Task_1/task_1.py
@@ -0,0 +1,22 @@
+import re
+str = input("Введіть рядок: ")
+numb = re.findall(r'\d+', str)
+str = re.sub(r"\d+", "", str, flags=re.UNICODE)
+numb = [int(i) for i in numb]
+print("\n Рядок без чисел:\n", str)
+print("\n Числа з рядка:\n", numb)
+str, result = str.title(), ""
+for word in str.split():    
+      result += word[:-1] + word[-1].upper() + " "
+print("\n Кожне слово починається і закінчується великою літерою:")
+print( " " + result[:-1])
+max_numb = max(numb)
+print("\n Максимальне число в масиві:", max_numb)
+arr_vals = []
+for i in range(len(numb)):
+    
+    if i !=numb.index(max_numb):
+        temp = numb[i] 
+        stup=temp** i
+        arr_vals.insert(i, stup)
+print("Масив чисел в степені по їх індексу:\n", arr_vals)
